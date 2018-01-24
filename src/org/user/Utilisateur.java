517db688f27fadec5e8853268bd62d52d4dc5974
blacksoulx5 <33748717+blacksoulx5@users.noreply.github.com>
2018-01-24 17:06:16 +0100

    Sam

---
+++ src/org/user/Utilisateur.java
@@ -0,0 +1,109 @@
+package org.user;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+
+/**
+ * Entity implementation class for Entity: Utilisateur
+ *
+ */
+@NamedQueries({
+	@NamedQuery(name = "UtilisateurFindByLogin", query = "SELECT u FROM Utilisateur u where u.email = :email AND u.password = :password") })
+
+@Entity
+public class Utilisateur implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String fname;
+	private String username;
+	private String email;
+	private String password;
+	
+	private static final long serialVersionUID = 1L;
+
+	public Utilisateur() {
+		super();
+	}
+
+	
+	public Utilisateur(String name, String fname, String username, String email, String password) {
+		super();
+		this.name = name;
+		this.fname = fname;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(final int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(final String fname) {
+		this.fname = fname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Utilisateur [id=" + id + ", email=" + email + ", fname=" + fname + ", username=" + username +", name=" + name + ", password="
+				+ password +"";
+	}
+	
+	
+
+}
