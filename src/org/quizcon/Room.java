0859e48a32a105111ea4795bbd2eb3c23338d120
IzeMath <32262467+IzeMath@users.noreply.github.com>
2018-01-29 19:05:43 +0100

    room

--- src/org/quizcon/Room.java
+++ src/org/quizcon/Room.java
@@ -4,58 +4,73 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  * Entity implementation class for Entity: Salon
  *
  */
+
+@NamedQueries({ @NamedQuery(name = "getListRoom", query = "SELECT r FROM Room r")})
+
 @Entity
 
 public class Room implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	private int id;
-	private String roomName;
+	private String name;
 	private int nbPlayers;
-	
-	
+	private String password;
+
 	public Room() {
 		super();
 	}
 
+	public Room(final int id, final String name, final String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
 
 	public int getId() {
 		return id;
 	}
 
-
 	public void setId(final int id) {
 		this.id = id;
 	}
 
-
-	public String getRoomName() {
-		return roomName;
+	public String getName() {
+		return name;
 	}
 
-
-	public void setRoomName(final String roomName) {
-		this.roomName = roomName;
+	public void setName(final String name) {
+		this.name = name;
 	}
-
 
 	public int getNbPlayers() {
 		return nbPlayers;
 	}
 
-
 	public void setNbPlayers(final int nbPlayers) {
 		this.nbPlayers = nbPlayers;
 	}
-	
-	
-   
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	public boolean isPrivate() {
+		return (this.password != null || this.password != "");
+	}
+
 }
