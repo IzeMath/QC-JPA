efb4d815e05f52fa3b02e0cf1e36a15cf889ad5d
IzeMath <32262467+IzeMath@users.noreply.github.com>
2018-02-13 14:35:18 +0100

    maj

--- src/org/quizcon/QuestionParameters.java
+++ src/org/quizcon/QuestionParameters.java
@@ -3,60 +3,64 @@ package org.quizcon;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class QuestionParameters implements Serializable{
+public class QuestionParameters implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private final ArrayList<String> arlg = new ArrayList<>();
-	private final ArrayList<String> arth = new ArrayList<>();
-	private final ArrayList<String> ardf = new ArrayList<>();
+	private ArrayList<String> arlg = new ArrayList<>();
+	private ArrayList<String> arth = new ArrayList<>();
+	private ArrayList<String> ardf = new ArrayList<>();
 	private int nbQuestion = 0;
-	
-	public QuestionParameters() {}
-	
-	
+
+	public QuestionParameters() {
+	}
+
 	public void addLang(final String lang) {
 		arlg.add(lang);
 	}
-	
+
 	public void addTheme(final String theme) {
 		arth.add(theme);
 	}
-	
+
 	public void addDifficulty(final String df) {
 		ardf.add(df);
 	}
-
 
 	public int getNbQuestion() {
 		return nbQuestion;
 	}
 
-
 	public void setNbQuestion(final int nbQuestion) {
 		this.nbQuestion = nbQuestion;
 	}
-
 
 	public ArrayList<String> getArlg() {
 		return arlg;
 	}
 
-
 	public ArrayList<String> getArth() {
 		return arth;
 	}
-
 
 	public ArrayList<String> getArdf() {
 		return ardf;
 	}
 
+	public void setArlg(final ArrayList<String> arlg) {
+		this.arlg = arlg;
+	}
+
+	public void setArth(final ArrayList<String> arth) {
+		this.arth = arth;
+	}
+
+	public void setArdf(final ArrayList<String> ardf) {
+		this.ardf = ardf;
+	}
 
 	@Override
 	public String toString() {
 		return "QuestionParameters [arlg=" + arlg + ", arth=" + arth + ", ardf=" + ardf + ", nbQuestion=" + nbQuestion
 				+ "]";
 	}
-	
-	
 
 }
--- src/org/quizcon/Room.java
+++ src/org/quizcon/Room.java
@@ -12,7 +12,8 @@ import javax.persistence.NamedQuery;
  *
  */
 
-@NamedQueries({ @NamedQuery(name = "getListRoom", query = "SELECT r FROM Room r")})
+@NamedQueries({ @NamedQuery(name = "getListRoom", query = "SELECT r FROM Room r"),
+		@NamedQuery(name = "canAccess", query = "SELECT r FROM Room r WHERE r.id = :id AND r.password = :password") })
 
 @Entity
 
